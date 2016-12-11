ace3a86a0c86be70080762dbaef4dcfae04943ab
Hsu <edittye379@gmail.com>
2016-12-12 06:58:03 +0800

    dry view!

--- views/index.view.php
+++ views/index.view.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Index</title>
-    </head>
-    <body>
-        <h1>Name: <?= $name ?></h1>
-        <?php
-            foreach ($tasks as $task) {
-               echo $task->description;
-            }
-        ?>
-    </body>
-</html>
+<?php require('partials/head.php'); ?>
+
+<h1>Name: <?= $name ?></h1>
+<?php
+    foreach ($tasks as $task) {
+       echo $task->description;
+    }
+?>
+
+<?php require('partials/footer.php'); ?>
