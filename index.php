94b6aa6ae3e2cce1eb7ad2b4b765659c68b16758
Hsu <edittye379@gmail.com>
2016-12-12 06:00:31 +0800

    core資料夾

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require 'function.php';
 // $fuck = ["123","213"];
 // dd($fuck);
 
-require 'bootstrap.php';
-require 'Task.php';
+require 'core/bootstrap.php';
 
+require 'Task.php';
 $tasks = $query->selectAll('todos', 'Task');
 
 foreach ($tasks as $task) {
